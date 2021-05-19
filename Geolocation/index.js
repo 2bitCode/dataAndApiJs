886eb5543750ef135318526a0f668ee2a1ea7973
2biCode <sachinb1377@gmail.com>
2021-05-19 13:16:48 +0545

    geolocation

--- Geolocation/index.js
+++ Geolocation/index.js
@@ -1,6 +1,6 @@
 // SERVER SIDE JS FILE
 
-const { json } = require("express");
+const { json, response } = require("express");
 const express = require("express");
 const DataStore = require("nedb");
 
@@ -12,16 +12,22 @@ app.use(express.json({ limit: "1mb" }));
 const database = new DataStore((filename = "database.db"));
 database.loadDatabase();
 
+app.get("/api", (request, response) => {
+  console.log("Got a get request!");
+  database.find({}, (err, data) => {
+    if (err) {
+      response.end();
+      return;
+    }
+    response.json(data);
+  });
+});
+
 app.post("/api", (request, response) => {
   console.log("I got request!");
-  const timeStamp = Date.now();
-  request.body.timeStamp = timeStamp;
+  const timeStamp = new Date();
+  request.body.timeStamp = timeStamp.toString();
   database.insert(request.body);
   console.log(request.body);
-  response.json({
-    status: "success",
-    latitude: request.body.lat,
-    longitude: request.body.lon,
-    timeStamp: request.body.timeStamp,
-  });
+  response.json(request.body);
 });
---
+++ Geolocation/public/list/list.js
@@ -0,0 +1,22 @@
+getData().catch((err) => console.log(err.message));
+
+async function getData() {
+  const response = await fetch("http://localhost:3000/api");
+  const data = await response.json();
+  console.log(data);
+
+  for (item of data) {
+    const root = document.querySelector(".container");
+    const latitude = document.createElement("div");
+    latitude.textContent = `Latitude: ${item.lat}`;
+    root.appendChild(latitude);
+    const longitude = document.createElement("div");
+    longitude.textContent = `Longitude: ${item.lon}`;
+    root.appendChild(longitude);
+    const timeStamp = document.createElement("div");
+    timeStamp.textContent = `TimeStamp: ${item.timeStamp}`;
+    root.appendChild(timeStamp);
+
+    document.body.append(root);
+  }
+}
