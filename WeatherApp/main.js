dc0a87df6b2369ba35eb831c1440a9d05a5e8494
2biCode <sachinb1377@gmail.com>
2021-05-20 14:27:23 +0545

    updated

---
+++ WeatherApp/main.js
@@ -0,0 +1,38 @@
+const position = {};
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(
+    (location) => {
+      console.log("async", position);
+      const api_key = "b111afc8f7de16f99a904c5af0357466";
+      const api_url = `http://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${api_key}&units=metric`;
+
+      async function getWeatherData() {
+        const response = await fetch(api_url);
+        const data = await response.json();
+        console.log(data);
+        // DOM MANIPULATION
+        const root = document.querySelector(".container");
+        root.textContent = null;
+        let flag = 1;
+        for (const item in data.main) {
+          const element = document.createElement("div");
+          element.textContent =
+            item == "temp" ||
+            item == "feels_like" ||
+            item == "temp_min" ||
+            item == "temp_max"
+              ? `🌈 ${item.toUpperCase()} : ${data.main[item]} C 🌈`
+              : `🌈 ${item.toUpperCase()} : ${data.main[item]} 🌈`;
+          root.appendChild(element);
+          console.log(item, data.main[item]);
+        }
+      }
+
+      getWeatherData();
+    },
+    (error) => console.log(error.message)
+  );
+} else {
+  console.log("Error!");
+}
